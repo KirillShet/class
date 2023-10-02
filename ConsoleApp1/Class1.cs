ce16227a3a9038fc4d9019da0b15f0c708b5093d
KirillShet <146167926+KirillShet@users.noreply.github.com>
2023-10-02 14:44:14 +0300

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Avto
+    {
+        public string nom;
+        public float bak;
+        public float ras;
+        public float speed;
+        public float put;
+        public int vrem;
+        public float top;
+        public int km;
+        public void info()
+        {
+            Console.WriteLine("Введите номер авто: ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Количество бензина в баке: ");
+            bak = float.Parse(Console.ReadLine());
+            Console.WriteLine("расход топлива на 100 м: ");
+            ras = float.Parse(Console.ReadLine());
+        }
+        public void output()
+        {
+            Console.WriteLine(nom);
+            Console.WriteLine(bak);
+            Console.WriteLine(ras);
+        }
+        public void zapravka()
+        {
+            Console.WriteLine("");
+            top = float.Parse(Console.ReadLine());
+        }
+
+        public void move()
+        {
+            Console.WriteLine("Авто едет на расстояние в случайном диапозоне от 1000 м до 15000");
+            Random rnd = new Random();
+            km = rnd.Next(1000, 15000);
+            Console.WriteLine("Авто проехало:" + km);
+            put = (km / 100) * ras;
+            if (bak < ras)
+            {
+                Console.WriteLine("Бензина в баке не хватит для того, чтобы доехать до нужного расстояния.");
+                Console.WriteLine("");
+            }
+        }
+
+       
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp1;
+using System.Data;
+using System.Net.Http.Headers;
+
+class programm
+{
+    static void Main()
+    {
+        Avto[] cars = new Avto[5];
+        for (int i = 0; i< cars.Length; i++)
+        {
+            cars[i] = new Avto();
+            cars[i].info();
+            cars[i].output();
+        }
+    }
+    static void rastoin()
+    {
+        Avto car = new Avto();
+        /*do
+        {
+
+        }
+        while () ;*/
+    }
+}
