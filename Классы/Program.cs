62cf729b9ec45a2698976e3122efeb565c07342b
KirillShet <146167926+KirillShet@users.noreply.github.com>
2023-10-05 13:38:33 +0300

    Add files via upload

---
+++ Классы/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+using System.Data;
+using System.Net.Http.Headers;
+
+class programm
+{
+    static void Main()
+    {
+        Avto[] cars = new Avto[1];
+        for (int i = 0; i< cars.Length; i++)
+        {
+            cars[i] = new Avto();
+            cars[i].info();
+            cars[i].output();
+        }
+        cars[0].output();
+        cars[0].move2();
+        cars[0].maxmove();
+        cars[0].boost();
+        cars[0].braking();
+    }
+}
